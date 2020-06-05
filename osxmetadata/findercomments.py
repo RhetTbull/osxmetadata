e96d63045a3cafc7bc1fedf23aa3581430095431
Borja González Seoane <garaje@glezseoane.es>
2020-06-05 11:56:51 +0200

    Fix directory exclusions when check existence
    Checking if a file name 'is a file', the directories were being
    excluded, when they are part of the scope. I use instead the 'exist'
    check of the 'os.path' library.
    
    To do the code concordant with the generality of the purpose (file and
    directories and not only files) I also change the variable names from
    'filename' to 'path'.
    
    This commit fixes issue #26.

--- osxmetadata/findercomments.py
+++ osxmetadata/findercomments.py
@@ -6,41 +6,41 @@ from . import _applescript
 
 _scpt_set_finder_comment = _applescript.AppleScript(
     """
-            on run {fname, fc}
-	            set theFile to fname
+            on run {path, fc}
+	            set thePath to path
 	            set theComment to fc
-	            tell application "Finder" to set comment of (POSIX file theFile as alias) to theComment
+	            tell application "Finder" to set comment of (POSIX file thePath as alias) to theComment
             end run
             """
 )
 
 _scpt_clear_finder_comment = _applescript.AppleScript(
     """
-            on run {fname}
-	            set theFile to fname
+            on run {path}
+	            set thePath to path
 	            set theComment to missing value
-	            tell application "Finder" to set comment of (POSIX file theFile as alias) to theComment
+	            tell application "Finder" to set comment of (POSIX file thePath as alias) to theComment
             end run
             """
 )
 
 
-def set_finder_comment(filename, comment):
-    """ set finder comment for filename
-        filename: path to file in posix format
+def set_finder_comment(path, comment):
+    """ set finder comment for object path (file or directory)
+        path: path to file or directory in posix format
         comment: comment string
     """
-    if not os.path.isfile(filename):
-        raise FileNotFoundError(f"Could not find {filename}")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Could not find {path}")
 
-    _scpt_set_finder_comment.run(filename, comment)
+    _scpt_set_finder_comment.run(path, comment)
 
 
-def clear_finder_comment(filename):
-    """ clear finder comment for filename
-        filename: path to file in posix format
+def clear_finder_comment(path):
+    """ clear finder comment for object path (file or directory)
+        path: path to file or directory in posix format
     """
-    if not os.path.isfile(filename):
-        raise FileNotFoundError(f"Could not find {filename}")
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Could not find {path}")
 
-    _scpt_clear_finder_comment.run(filename)
+    _scpt_clear_finder_comment.run(path)
